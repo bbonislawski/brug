8ba9deebb3c43ae408f920184f0976c3f72add7c
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-01-23 14:15:03 +0100

    create sign in form

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,3 +1,6 @@
-get '/users/new', to: 'users#new'
 root           to: 'dashboard#index'
+
+get '/users/new', to: 'users#new'
 post '/users', to: 'users#create'
+
+get '/sessions/new', to: 'sessions#new'
---
+++ spec/web/views/sessions/new_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require_relative '../../../../apps/web/views/sessions/new'
+
+describe Web::Views::Sessions::New do
+  let(:exposures) { Hash[] }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/sessions/new.html.erb') }
+  let(:view)      { Web::Views::Sessions::New.new(template, exposures) }
+  let(:rendered)  { view.render }
+
+  it 'renders sign in message' do
+    rendered.must_include('<h2>Sign In</h2>')
+  end
+end
