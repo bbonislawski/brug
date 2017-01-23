c060e26539b780e10e5ea107d1362ddfe06e8356
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-01-23 14:43:57 +0100

    implement find_and_authenticate in user repository

--- lib/brug/repositories/user_repository.rb
+++ lib/brug/repositories/user_repository.rb
@@ -1,6 +1,16 @@
 class UserRepository < Hanami::Repository
-  def create_with_hashed_password(data)
-    with_hashed_password = data.merge(password: BCrypt::Password.create(data[:password]))
+  def create_with_hashed_password(form)
+    with_hashed_password = form.merge(password: BCrypt::Password.create(form[:password]))
     self.create(with_hashed_password)
+  end
+
+  def find_and_authenticate(form)
+    user = users.where(email: form[:user][:email]).first
+    return nil unless user
+    if BCrypt::Password.new(user.password) == form[:user][:password]
+      user
+    else
+      nil
+    end
   end
 end
--- spec/brug/repositories/user_repository_spec.rb
+++ spec/brug/repositories/user_repository_spec.rb
@@ -12,4 +12,34 @@ class TestUserRepository < Minitest::Test
     assert_equal email, entity.email
     assert(BCrypt::Password.new(entity.password) == password)
   end
+
+  def test_success_find_and_authenticate_user
+    @repo.clear
+    email = 'test@test.test'
+    password = 'passw0rd'
+    created_user = @repo.create_with_hashed_password(email: email, password: password)
+
+    found_user = @repo.find_and_authenticate({ user: { email: email,
+                                                            password: password } })
+
+    assert_equal created_user.id, found_user.id
+  end
+
+  def test_find_and_authenticate_user_with_wrong_password
+    @repo.clear
+    email = 'test@test.test'
+    password = 'passw0rd'
+    created_user = @repo.create_with_hashed_password(email: email, password: password)
+
+    found_user = @repo.find_and_authenticate({ user: { email: email,
+                                                            password: 'invalid' } })
+    assert_nil found_user
+  end
+
+  def test_find_and_authenticate_user_with_wrong_email
+    @repo.clear
+    found_user = @repo.find_and_authenticate({ user: { email: 'not@existing.mail',
+                                                            password: 'invalid' } })
+    assert_nil found_user
+  end
 end
