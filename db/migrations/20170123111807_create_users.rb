8f611bac0e5d4151e804788ab016a78bfa417490
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-01-24 01:44:15 +0100

    set id to be primary key

--- db/migrations/20170123111807_create_users.rb
+++ db/migrations/20170123111807_create_users.rb
@@ -2,7 +2,7 @@ Hanami::Model.migration do
   up do
     execute 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
     create_table :users do
-      column :id, 'uuid', null: false, default: Hanami::Model::Sql.function(:uuid_generate_v4)
+      primary_key :id, 'uuid', null: false, default: Hanami::Model::Sql.function(:uuid_generate_v4)
       column :email, String
       column :password, String
     end
